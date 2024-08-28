398a296a7acf15d09254ec4b16ad160196f5b3ea
SAYAN-DEV-NATH <sayandevnath01@gmail.com>
2024-08-28 19:59:21 +0600

    Initial commit

---
+++ app/src/main/java/com/sayan/picasso/MainActivity.java
@@ -0,0 +1,28 @@
+package com.sayan.picasso;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+
+        Picasso.get().load("https://www.shutterstock.com/image-photo/natural-tanguar-haor-sylhet-bangladesh-areal-2356112995").into(imageView);
+
+    }
+}
